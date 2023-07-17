30b4ef02cce2e7ff34e33034323a3017c714ae55
andrijaivkovic <andrija.ivkovic@outlook.com>
2023-07-17 03:29:34 +0200

    user selected theme persists on page reload

--- src/App.js
+++ src/App.js
@@ -10,53 +10,10 @@ import TodoList from "./components/TodoList/TodoList";
 import TodoListStats from "./components/TodoListStats/TodoListStats";
 import TodoListFilter from "./components/TodoListFilter/TodoListFilter";
 
-// const lightTheme = {
-//   "--color-primary": "hsl(220, 98%, 61%)",
-//   "--color-gradient-1": "hsl(192, 100%, 67%)",
-//   "--color-gradient-2": "hsl(280, 87%, 65%)",
-//   "--color-bg-1": "hsl(0, 0%, 98%)",
-//   "--color-bg-2": "hsl(236, 33%, 92%)",
-//   "--color-text-1": "hsl(233, 11%, 84%)",
-//   "--color-text-2": "hsl(236, 9%, 61%)",
-//   "--color-text-3": "hsl(235, 19%, 35%)",
-// };
-
-// const darkTheme = {
-//   "--color-bg-1": "hsl(235, 21%, 11%)",
-//   "--color-bg-2": "hsl(235, 24%, 19%)",
-//   "--color-text-1": "hsl(234, 39%, 85%)",
-//   "--color-text-2": "hsl(234, 11%, 52%)",
-//   "--color-text-3": "hsl(237, 14%, 26%)",
-// };
-
-// const applyTheme = (nextTheme) => {
-//   const theme = nextTheme === "dark" ? darkTheme : lightTheme;
-//   Object.keys(theme).forEach((key) => {
-//     const value = theme[key];
-//     document.documentElement.style.setProperty(key, value);
-//   });
-// };
-
 const App = () => {
-  // const tempItems = [
-  //   {
-  //     id: uuidv4(),
-  //     text: "Complete online JavaScript course",
-  //     isCompleted: false,
-  //   },
-  //   { id: uuidv4(), text: "Jog around park 3x", isCompleted: false },
-  //   { id: uuidv4(), text: "10 minutes meditation", isCompleted: false },
-  //   { id: uuidv4(), text: "Read for 1 hour", isCompleted: false },
-  //   { id: uuidv4(), text: "Pick up groceries", isCompleted: false },
-  //   {
-  //     id: uuidv4(),
-  //     text: "Complete Todo App on Frontend Mentor",
-  //     isCompleted: false,
-  //   },
-  // ];
+  const [currentTheme, setTheme] = useState(getInitialTheme);
 
-  const [currentTheme, setTheme] = useState("dark");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getInitialItems);
 
   const [filteredBy, setFilteredBy] = useState("all");
   const [filteredItems, setFilteredItems] = useState([]);
@@ -65,6 +22,17 @@ const App = () => {
     return !item.isCompleted ? acc + 1 : acc;
   }, 0);
 
+  function getInitialTheme() {
+    const data = localStorage.getItem("APP_THEME");
+    const savedTheme = JSON.parse(data);
+    return savedTheme || "light";
+  }
+
+  useEffect(() => {
+    const data = JSON.stringify(currentTheme);
+    localStorage.setItem("APP_THEME", data);
+  }, [currentTheme]);
+
   useEffect(() => {
     document.documentElement.className = "";
     document.documentElement.classList.add(
@@ -72,21 +40,17 @@ const App = () => {
     );
   }, [currentTheme]);
 
-  // automatically sets the theme from state on page load
-  // applyTheme(currentTheme);
-
   useEffect(() => {
     handleFilter(filteredBy);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [items]);
 
-  // loads the data from localStorage and (if it exists) sets it as the state array
-  useEffect(() => {
-    const data = window.localStorage.getItem("APP_STATE");
-    if (data !== null) setItems(JSON.parse(data));
-  }, []);
+  function getInitialItems() {
+    const data = localStorage.getItem("APP_STATE");
+    const savedTodos = JSON.parse(data);
+    return savedTodos || [];
+  }
 
-  // updates the data in localStorage
   useEffect(() => {
     window.localStorage.setItem("APP_STATE", JSON.stringify(items));
   }, [items]);
@@ -124,7 +88,6 @@ const App = () => {
   const handleFilter = (filterBy) => {
     if (filterBy === "all") {
       setFilteredBy(filterBy);
-      // setFilteredItems([]);
     }
 
     if (filterBy === "active") {
@@ -139,7 +102,7 @@ const App = () => {
     if (filterBy === "completed") {
       const newFilteredItems = items
         .filter((item) => (item.isCompleted ? item : null))
-        .map((item) => item); // console.log(filteredItems, filteredBy);
+        .map((item) => item);
 
       setFilteredItems(newFilteredItems);
       setFilteredBy(filterBy);
